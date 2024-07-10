ecf3347d1df56f9ff403ddf0fe01886ddfcb0c39
kazuya kawaguchi <kawakazu80@gmail.com>
2024-07-10 15:46:40 +0900

    setup traverser for transform

--- src/transform.ts
+++ src/transform.ts
@@ -1,13 +1,31 @@
 import { IRNodeTypes } from '@vue-vapor/compiler-vapor'
+import { extend, isArray } from '@vue/shared'
 import { newBlock } from './transforms/utils'
 
+import type { CompilerCompatOptions } from '@vue-vapor/compiler-dom'
 import type { RootIRNode, RootNode } from './ir'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type TransformOptions = any // TODO:
+const defaultOptions = {}
+
+// TODO:
+export class TransformContext<T = unknown> {
+  root: TransformContext<RootNode>
+
+  options: Required<Omit<TransformOptions, 'filename' | keyof CompilerCompatOptions>>
+  constructor(
+    public ir: RootIRNode,
+    public node: T,
+    options: TransformOptions = {}
+  ) {
+    this.options = extend({}, defaultOptions, options)
+    this.root = this as TransformContext<RootNode>
+  }
+}
 
 // svelte-AST -> IR
-export function transform(node: RootNode, _options: TransformOptions = {}): RootIRNode {
+export function transform(node: RootNode, options: TransformOptions = {}): RootIRNode {
   const ir: RootIRNode = {
     type: IRNodeTypes.ROOT,
     node,
@@ -18,5 +36,46 @@ export function transform(node: RootNode, _options: TransformOptions = {}): Root
     block: newBlock(node)
   }
 
+  const context = new TransformContext(ir, node, options)
+
+  transformNode(context)
+
   return ir
+}
+
+export function transformNode(context: TransformContext): void {
+  let { node } = context
+
+  // apply transform plugins
+  const { nodeTransforms } = context.options
+  const exitFns = []
+  for (const nodeTransform of nodeTransforms) {
+    const onExit = nodeTransform(node, context)
+    if (onExit) {
+      if (isArray(onExit)) {
+        exitFns.push(...onExit)
+      } else {
+        exitFns.push(onExit)
+      }
+    }
+    if (context.node) {
+      // node may have been replaced
+      node = context.node
+    } else {
+      // node was removed
+      return
+    }
+  }
+
+  // exit transforms
+  context.node = node
+  let i = exitFns.length
+  while (i--) {
+    exitFns[i]()
+  }
+
+  // @ts-expect-error -- TODO
+  if (context.node.type === IRNodeTypes.ROOT) {
+    // context.registerTemplate()
+  }
 }
