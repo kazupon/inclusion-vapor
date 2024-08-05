0d68b8f90846ec2e78f212ccf7fc021386dbd609
kazuya kawaguchi <kawakazu80@gmail.com>
2024-08-05 16:42:37 +0900

    fix: not work playground

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,6 +1,7 @@
 import { createVaporApp as createApp } from 'vue/vapor'
 import './style.css'
-import App from './App'
-// import App from './App.vue'
+import App from './App.vue'
 
+// @ts-expect-error -- NOTE: ignore, because we are still prototyping ...
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 createApp(App, {}).mount('#app')
--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,12 +1,23 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import inspect from 'vite-plugin-inspect'
 import * as CompilerSFC from '@vue/compiler-sfc'
 
+const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)))
+const resolve = (p: string) => path.resolve(dirname, './node_modules', p)
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      'vue/vapor': resolve('vue/vapor/index.mjs')
+    }
+  },
   plugins: [
     vue({
+      vapor: true,
       compiler: CompilerSFC
     }),
     inspect()
