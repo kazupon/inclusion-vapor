0fd939d404fe0421179b17de3d7baf262f0b8762
kazuya kawaguchi <kawakazu80@gmail.com>
2024-08-23 15:53:11 +0900

    chore: remove comment

--- packages/unplugin-react/src/types.ts
+++ packages/unplugin-react/src/types.ts
@@ -1,5 +1,4 @@
 export interface Options {
   include?: string | RegExp | (string | RegExp)[]
   exclude?: string | RegExp | (string | RegExp)[]
-  // TODO: we shouldl add more `vite-plugin-svelte` options
 }
