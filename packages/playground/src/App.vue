8c0a101b6a89d8a33181722488ee1c9ebafc6dd8
kazuya kawaguchi <kawakazu80@gmail.com>
2024-08-26 13:12:15 +0900

    fix: update playground

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import VueContainer from './components/VueContainer.vue'
 import SvelteContainer from './components/SvelteContainer.svelte'
+// import ReactCounter from './components/Counter.tsx'
 </script>
 
 <template>
--- packages/playground/src/components/Counter.tsx
+++ packages/playground/src/components/Counter.tsx
@@ -1,1 +1,12 @@
-console.log('TODO: React counter!')
+import { useState } from 'react'
+
+function Counter() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="card">
+      <button onClick={() => setCount(count + 1)}>React Vapor count is {count}</button>
+    </div>
+  )
+}
+
+export default Counter
