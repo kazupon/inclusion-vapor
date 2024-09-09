ae3c001b3b715761728df58fc5c998c56093f16c
kazuya kawaguchi <kawakazu80@gmail.com>
2024-09-09 19:18:16 +0900

    test(svelte-template-compiler): add test case and basic testing

--- packages/svelte-template-compiler/src/transforms/vIf.test.ts
+++ packages/svelte-template-compiler/src/transforms/vIf.test.ts
@@ -1,10 +1,62 @@
+import { NodeTypes } from '@vue-vapor/compiler-dom'
+import { compile as vaporCompile } from '@vue-vapor/compiler-vapor'
 import { expect, test } from 'vitest'
+import { IRNodeTypes } from '../ir/index.ts'
+import { makeCompile } from './_utils.ts'
+import { transformChildren } from './transformChildren.ts'
+import { transformElement } from './transformElement.ts'
+import { transformText } from './transformText.ts'
+import { transformVBind } from './vBind.ts'
+import { transformVIf } from './vIf.ts'
 
-test.todo('basic', () => {
+import type { IfIRNode } from '../ir/index.ts'
+
+const compileWithVIf = makeCompile({
+  nodeTransforms: [transformElement, transformChildren, transformText, transformVIf],
+  directiveTransforms: {
+    bind: transformVBind
+  }
+})
+
+test('basic', () => {
   const source = `{#if ok}
   <div>{msg}</div>
 {/if}`
-  expect(source).toBe('todo')
+  const { code, vaporHelpers, ir, helpers } = compileWithVIf(source)
+  const expectedResult = vaporCompile(`<div v-if="ok">{{msg}}</div>`)
+  expect(code).toMatchSnapshot('received')
+  expect(expectedResult.code).toMatchSnapshot('expected')
+  expect(vaporHelpers).toEqual(expectedResult.vaporHelpers)
+
+  expect(vaporHelpers).contains('createIf')
+  expect(helpers.size).toBe(0)
+
+  expect(ir.template).toEqual(['<div></div>'])
+  expect(ir.block.operation).toMatchObject([
+    {
+      type: IRNodeTypes.IF,
+      id: 0,
+      condition: {
+        type: NodeTypes.SIMPLE_EXPRESSION,
+        content: 'ok',
+        isStatic: false
+      },
+      positive: {
+        type: IRNodeTypes.BLOCK,
+        dynamic: {
+          children: [{ template: 0 }]
+        }
+      }
+    }
+  ])
+  expect(ir.block.returns).toEqual([0])
+
+  expect(ir.block.dynamic).toMatchObject({
+    children: [{ id: 0 }]
+  })
+
+  expect(ir.block.effect).toEqual([])
+  expect((ir.block.operation[0] as IfIRNode).positive.effect).lengthOf(1)
 })
 
 test.todo('dedupe same template', () => {
@@ -53,6 +105,22 @@ test.todo('comment between blocks', () => {
 {:else if orNot}
   <!-- bar -->
   <p></p>
+{:else}
+  <template>fine</template>
+{/if}`
+  expect(source).toBe('todo')
+})
+
+test.todo('nested #if', () => {
+  const source = `{#if ok}
+  <p>top</p>
+  {#if nested}
+    <span>nested</span>
+  {:else if nestedElse}
+    <span>nestedElseIf</span>
+  {:else}
+    <span>nestedElse</span>
+  {/if}
 {:else}
   <template>fine</template>
 {/if}`
