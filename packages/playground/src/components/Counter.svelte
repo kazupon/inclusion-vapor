fb2e7c729951c87acb2735c3f9143aed7473733a
kazuya kawaguchi <kawakazu80@gmail.com>
2024-11-10 18:24:48 +0900

    fix(playground): update

--- packages/playground/src/components/Counter.svelte
+++ packages/playground/src/components/Counter.svelte
@@ -2,24 +2,36 @@
   import { createEventDispatcher, onMount } from 'svelte'
   import { writable } from 'svelte/store'
 
-  let count = writable(0)
+  let count = 1
+  const store = writable(count)
   const dispatch = createEventDispatcher()
 
   onMount(() => {
-    console.log('Svelte Vapor component mounted', $count)
+    console.log('Svelte Vapor component mounted', count)
   })
+
+  $: double = count * 2
+  $: {
+    console.log('count', count)
+    console.log('double', double)
+    store.set(count)
+  }
 
   function increment(e) {
     console.log('increment svelte', e)
-    count.set($count + 1)
-    dispatch('increment', { count: $count })
+    count++
+    dispatch('increment', { count: count })
   }
 </script>
 
 <div class="box" id="svelte">
   <button on:click={increment}>
-    Svelte Vapor count is {$count}
+    Svelte Vapor count is {count}
   </button>
+  <br />
+  <span>Double: {double}</span>
+  <br />
+  <span>Store: {$store}</span>
   <br />
   <slot></slot>
 </div>
